aaf37ebbb9c81748e8bef09b798fdf7e3bd6e0bb
Amiculay <aaronnguyen1123@gmail.com>
2022-07-10 18:54:24 -0400

    Initial commit

---
+++ Day6/tabs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Tabs from './Components/Tabs'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Successful reset</h1>
+      <Tabs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day6/tabs/src/Components/Tabs.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const Tabs = () => {
+    return (
+        <>
+            <h1>Successful link :D</h1>
+        </>
+    )
+}
+
+export default Tabs
