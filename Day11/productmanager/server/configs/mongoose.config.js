65da6755b3716d1fea876fad98ac0b30560b4f26
Aaron Nguyen <104883016+aaronpnguyen@users.noreply.github.com>
2022-07-19 00:45:53 -0400

    Delete Day11/productmanager directory

--- Day11/productmanager/server/configs/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-DATABASE = "products_db";
-
-mongoose.connect(`mongodb+srv://root:root@july2022.shwr2gh.mongodb.net/${DATABASE}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log(`Established a connection to ${DATABASE}!`))
-    .catch(error => console.log(`Something went wrong when connecting to the database `, error))
